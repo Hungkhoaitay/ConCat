a3e119bb2ae0acce74d24fe96129d12f81df6dec
hmanhduc2k <hmanhduc2k@gmail.com>
2021-07-16 13:48:22 +0800

    Navigation Drawer setup

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,21 @@
  package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.app.ActionBar;
+import android.app.Notification;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
@@ -12,17 +24,23 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.HashMap;
 import java.util.Set;
 
 import static android.content.ContentValues.TAG;
 
- public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+ public class MainActivity extends AppCompatActivity
+         implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
     final static int[] buttonIDs = new int[] { R.id.firstButton, R.id.secondButton, R.id.thirdButton, R.id.fourthButton };
     public static final String SHARED_PREFS = "sharedPrefs";
@@ -32,6 +50,12 @@ import static android.content.ContentValues.TAG;
 
     private SharedPreferences sharedPreferences;
 
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,9 +63,29 @@ import static android.content.ContentValues.TAG;
         sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
 
         loadData();
+        setUpDrawer();
 
         Button conCat = findViewById(R.id.ConCat);
         conCat.setOnClickListener(this);
+
+    }
+
+    private void setUpDrawer() {
+        this.drawerLayout = findViewById(R.id.drawerLayout);
+        this.toolbar = findViewById(R.id.drawer_toolbar);
+        setSupportActionBar(toolbar);
+        this.navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        this.actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
     }
 
     private void askForSystemOverlayPermission() {
@@ -114,5 +158,19 @@ import static android.content.ContentValues.TAG;
      protected void onDestroy() {
          super.onDestroy();
          PERMISSION_GRANTED = false;
+     }
+
+     @Override
+     public boolean onNavigationItemSelected(MenuItem item) {
+         switch (item.getItemId()) {
+             case R.id.item1:
+
+             case R.id.item2:
+
+             case R.id.item3:
+                 return true;
+             default:
+                 return false;
+         }
      }
  }
