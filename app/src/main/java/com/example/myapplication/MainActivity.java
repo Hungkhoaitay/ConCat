e513cbd5b44626304da620e9fd51d631aaea12c6
hmanhduc2k <hmanhduc2k@gmail.com>
2021-07-16 13:48:22 +0800

    Navigation Drawer setup

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,21 @@
-package com.example.myapplication;
+ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.app.ActionBar;
+import android.app.Notification;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
@@ -13,36 +24,25 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
-import com.example.myapplication.databinding.ActivityScrollingBinding;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.HashMap;
+import java.util.Set;
 
 import static android.content.ContentValues.TAG;
 
- public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity
+         implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
-    final static int[] buttonIDs = new int[] { R.id.firstButton, R.id.secondButton, R.id.thirdButton, R.id.fourthButton, R.id.fifthButton, R.id.sixthButton };
+    final static int[] buttonIDs = new int[] { R.id.firstButton, R.id.secondButton, R.id.thirdButton, R.id.fourthButton };
     public static final String SHARED_PREFS = "sharedPrefs";
 
     private static final int WINDOW_PERMISSION = 123;
@@ -50,47 +50,45 @@ import static android.content.ContentValues.TAG;
 
     private SharedPreferences sharedPreferences;
 
-    private FirebaseDatabase rootNode;
-    private DatabaseReference reference;
 
-     @Override
-     protected void onStart() {
-         super.onStart();
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
 
-         // user = mAuth.getCurrentUser();
-     }
-
-     @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        // this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        this.setContentView(R.layout.activity_main);
-
-//        rootNode = FirebaseDatabase.getInstance();
-//        reference = rootNode.getReference("user");
-//        reference.child(user.getUid());
-
+        setContentView(R.layout.activity_main);
         sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+
         loadData();
+        setUpDrawer();
 
-//        Button conCat = findViewById(R.id.ConCat);
-//        conCat.setOnClickListener(this);
+        Button conCat = findViewById(R.id.ConCat);
+        conCat.setOnClickListener(this);
 
-         FloatingActionButton accountBtn = findViewById(R.id.accountBtn);
-         accountBtn.setOnClickListener(view -> {
-             Intent intent = new Intent(MainActivity.this, AccountActivity.class);
-             MainActivity.this.startActivity(intent);
-         });
-
-         Button customizeBtn = findViewById(R.id.customizeBtn);
-         customizeBtn.setOnClickListener(view -> {
-             Intent intent = new Intent(MainActivity.this, CustomizeActivity.class);
-             MainActivity.this.startActivity(intent);
-         });
     }
 
-     private void askForSystemOverlayPermission() {
+    private void setUpDrawer() {
+        this.drawerLayout = findViewById(R.id.drawerLayout);
+        this.toolbar = findViewById(R.id.drawer_toolbar);
+        setSupportActionBar(toolbar);
+        this.navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        this.actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+    }
+
+    private void askForSystemOverlayPermission() {
         Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
                 Uri.parse("package:" + getPackageName()));
         startActivityForResult(intent, WINDOW_PERMISSION);
@@ -100,30 +98,43 @@ import static android.content.ContentValues.TAG;
     }
 
 
-//    @Override
-//    public void onClick(View v) {
-//        TextView display = findViewById(R.id.display);
-//        switch (v.getId()) {
-//            case R.id.ConCat:
-//                if (PERMISSION_GRANTED == false) {
-//                    askForSystemOverlayPermission();
-//                    break;
-//                }
-//                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(this)) {
-//                    Intent intent = new Intent(MainActivity.this, FloatingViewService.class);
-//                    intent.putExtra("North", sharedPreferences.getString(Integer.toString(buttonIDs[0]), AppInfo.EMPTY));
-//                    intent.putExtra("South", sharedPreferences.getString(Integer.toString(buttonIDs[1]), AppInfo.EMPTY));
-//                    intent.putExtra("East", sharedPreferences.getString(Integer.toString(buttonIDs[2]), AppInfo.EMPTY));
-//                    intent.putExtra("West", sharedPreferences.getString(Integer.toString(buttonIDs[3]), AppInfo.EMPTY));
-//                    startService(intent);
-//                } else {
-//                    askForSystemOverlayPermission();
-//                }
-//                break;
-//            default:
-//                break;
-//        }
-//    }
+    @Override
+    public void onClick(View v) {
+        TextView display = findViewById(R.id.display);
+        switch (v.getId()) {
+            case R.id.ConCat:
+                if (PERMISSION_GRANTED == false) {
+                    askForSystemOverlayPermission();
+                    break;
+                }
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(this)) {
+                    Intent intent = new Intent(MainActivity.this, FloatingViewService.class);
+                    intent.putExtra("North", sharedPreferences.getString(Integer.toString(buttonIDs[0]), AppInfo.EMPTY));
+                    intent.putExtra("South", sharedPreferences.getString(Integer.toString(buttonIDs[1]), AppInfo.EMPTY));
+                    intent.putExtra("East", sharedPreferences.getString(Integer.toString(buttonIDs[2]), AppInfo.EMPTY));
+                    intent.putExtra("West", sharedPreferences.getString(Integer.toString(buttonIDs[3]), AppInfo.EMPTY));
+                    startService(intent);
+                } else {
+                    askForSystemOverlayPermission();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+     /**
+      * save the states of the buttons
+      */
+//     public void saveData() {
+//         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+//         SharedPreferences.Editor editor = sharedPreferences.edit();
+//
+//         for (int btnID: buttonIDs) {
+//             editor.putString(Integer.toString(btnID), buttonID2appName.get(btnID));
+//         }
+//         editor.apply();
+//     }
 
      public void loadData() {
          for (int btnID: buttonIDs) {
@@ -147,5 +158,19 @@ import static android.content.ContentValues.TAG;
      protected void onDestroy() {
          super.onDestroy();
          PERMISSION_GRANTED = false;
+     }
+
+     @Override
+     public boolean onNavigationItemSelected(MenuItem item) {
+         switch (item.getItemId()) {
+             case R.id.item1:
+
+             case R.id.item2:
+
+             case R.id.item3:
+                 return true;
+             default:
+                 return false;
+         }
      }
  }
