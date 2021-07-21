069cba55db1b6939a528ab9964058b5901f69474
hmanhduc2k <hmanhduc2k@gmail.com>
2021-07-21 12:32:53 +0800

    Changing dependencies and add test cases

--- app/src/androidTest/java/com/example/myapplication/ApplicationSetUpTest.java
+++ app/src/androidTest/java/com/example/myapplication/ApplicationSetUpTest.java
@@ -1,47 +1,57 @@
 package com.example.myapplication;
 
-import androidx.core.content.ContextCompat;
-
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.app.Service;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+
 import android.os.Build;
 import android.os.IBinder;
+
 import android.support.test.InstrumentationRegistry;
 
-import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.ext.truth.content.IntentSubject.assertThat;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.filters.LargeTest;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.rule.ActivityTestRule;
 import androidx.test.rule.ServiceTestRule;
 
 import org.junit.runner.RunWith;
-
-import android.app.Service;
-import android.app.ActivityManager;
-import android.app.ActivityManager.RunningServiceInfo;
-import android.content.Intent;
 
 import org.junit.*;
 import org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.MockitoRule;
+
 /*
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-
  */
 
 import java.util.concurrent.TimeoutException;
@@ -62,11 +72,8 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
@@ -82,39 +89,48 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
-/**
- * List of things needed to do an instrumented unit test on:
- * 1. Direction of floating widget determined by checkRegion
- * 2. Click, long-click, and move condition determined by checkIfMove
- * 3. Visibility state of floating widget
- * 4. Application intent and package name received is launch-able
- * 5. Application is launched as specified
- * 6. Notification testing
- * 7. The specified direction receives the correct launch intent
- * 8. If String app package is null or launch intent is null, it is handled
- *
- * https://androidx.de/androidx/test/rule/ServiceTestRule.html
- */
 @Ignore
+@LargeTest
 @RunWith(AndroidJUnit4.class)
-public class FloatingViewUITest {
+@SdkSuppress(minSdkVersion = 26)
+public class ApplicationSetUpTest {
+
+    private static final String SAMPLE_PACKAGE =
+            "com.example.myapplication";
+
+    private static final int LAUNCH_TIMEOUT = 5000;
+    private UiDevice device;
 
     @Rule
-    ServiceTestRule serviceTestRule = new ServiceTestRule();
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Before
-    public void setup() {
+    public void startMainActivity() {
+        this.device = UiDevice.getInstance(getInstrumentation());
+        device.pressHome();
 
+        final Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+
+        PackageManager packageManager = getApplicationContext().getPackageManager();
+        ResolveInfo resolveInfo = packageManager.resolveActivity(intent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        String launcherPackage = resolveInfo.activityInfo.packageName;
+        Log.i("Package name: ", launcherPackage);
+        assertThat(launcherPackage).isNotNull();
+        // Launch the blueprint app
+        Context context = getApplicationContext();
+        final Intent intent1 = context.getPackageManager()
+                .getLaunchIntentForPackage(SAMPLE_PACKAGE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);    // Clear out any previous instances
+        context.startActivity(intent1);
+
+        device.wait(Until.hasObject(By.pkg(SAMPLE_PACKAGE).depth(0)), LAUNCH_TIMEOUT);
     }
-    /**
-     * Assert that all specified component are visible and working
-     * @throws TimeoutException
-     */
-    @Test
-    public void onCreateTest() throws TimeoutException {
-        FloatingViewService floatingViewService = Mockito.mock(FloatingViewService.class);
 
-        assertThat(floatingViewService).isNotNull();
-        // assertThat(floatingViewService.mFloatingView.getVisibility()).isEqualTo(View.VISIBLE);
+    // Confirm that application is launched on call
+    @Test
+    public void checkPreCondition() {
+        assertThat(device).isNotNull();
     }
 }
