d0fc8e946bad34126a51be028f6d8dd2b2b4ee3a
hmanhduc2k <hmanhduc2k@gmail.com>
2021-07-25 20:51:57 +0800

    Test Login Activity

---
+++ app/src/main/java/com/example/myapplication/StringValue.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+public class StringValue {
+
+    public static final String DEFAULT_VALUE = "DEFAULT";
+    public static final String CANCEL_SELECTION = "CANCEL_SELECTION";
+    public static final String IMAGE_SELECTION = "IMAGE SELECTION";
+
+    public static final int REGION_1 = 0;
+    public static final int REGION_2 = 1;
+    public static final int REGION_3 = 2;
+    public static final int REGION_4 = 3;
+    public static final int REGION_5 = 4;
+    public static final int REGION_6 = 5;
+
+}
